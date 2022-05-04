276688b3f3dbeec7e14b5ebea684548b07224fc0
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-05-04 18:24:51 +0300

    Initial Commit

---
+++ MedicalCardVersion2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicalCardVersion2
+//
+//  Created by Вячеслав Гудзенко on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
