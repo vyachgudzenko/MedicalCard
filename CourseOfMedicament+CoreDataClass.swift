d03313cb86e3b951b4a32dbdb18e8e5475b51b19
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-07-02 00:05:31 +0300

    Added model CourseOfMedicament

---
+++ CourseOfMedicament+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CourseOfMedicament+CoreDataClass.swift
+//  MedicalCardVersion2
+//
+//  Created by Вячеслав Гудзенко on 02.07.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CourseOfMedicament)
+public class CourseOfMedicament: NSManagedObject {
+
+}
---
+++ CourseOfMedicament+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  CourseOfMedicament+CoreDataProperties.swift
+//  MedicalCardVersion2
+//
+//  Created by Вячеслав Гудзенко on 02.07.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CourseOfMedicament {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CourseOfMedicament> {
+        return NSFetchRequest<CourseOfMedicament>(entityName: "CourseOfMedicament")
+    }
+
+    @NSManaged public var itsDrunk: Bool
+    @NSManaged public var section: String?
+    @NSManaged public var medicament: Medicament?
+
+}
+
+extension CourseOfMedicament : Identifiable {
+
+}
