1854752c974a3ff5b625047b5618fc811d1608e0
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-07-19 03:21:45 +0300

    create FlterButton

---
+++ MedicalCardVersion2/Views/FilterButton.swift
@@ -0,0 +1,40 @@
+//
+//  FilterButton.swift
+//  MedicalCardVersion2
+//
+//  Created by Вячеслав Гудзенко on 19.07.2022.
+//
+
+import UIKit
+
+class FilterButton: UIButton {
+    
+    var flag:Bool = false {
+        didSet{
+            if flag{
+                self.setTitleColor(.white, for: .normal)
+                self.layer.backgroundColor = UIColor.systemPink.cgColor
+            } else{
+                self.setTitleColor(.systemPink, for: .normal)
+                self.layer.backgroundColor = UIColor.white.cgColor
+            }
+            self.setNeedsDisplay()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 15
+        self.layer.borderColor = UIColor.systemPink.cgColor
+        self.layer.borderWidth = 1
+        if flag{
+            self.setTitleColor(.white, for: .normal)
+            self.layer.backgroundColor = UIColor.systemPink.cgColor
+        } else{
+            self.setTitleColor(.systemPink, for: .normal)
+            self.layer.backgroundColor = UIColor.white.cgColor
+        }
+    }
+    
+}
--- MedicalCardVersion2/Views/Views/Filters/FilterController.swift
+++ MedicalCardVersion2/Views/Views/Filters/FilterController.swift
@@ -12,6 +12,39 @@ class FilterController: UIViewController {
     
     @IBOutlet weak var baseView: UIView!
     
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    /*@IBOutlet weak var dateButton: FilterButton!
+    
+    @IBOutlet weak var alphabetButton: FilterButton!
+    
+    
+    @IBAction func dateButtonTapped(_ sender: UIButton) {
+        dateButton.flag = !dateButton.flag
+        if dateButton.flag{
+            alphabetButton.flag = false
+        }
+    }
+    @IBAction func alphabetButtonTapped(_ sender: Any) {
+        alphabetButton.flag = !alphabetButton.flag
+        if alphabetButton.flag{
+            dateButton.flag = false
+        }
+    }*/
+    
+    var dateCreateButton:FilterButton! = {
+        let button = FilterButton()
+        button.setTitle("Date", for: .normal)
+        return button
+    }()
+    
+    var alphabetButton:FilterButton! = {
+        let button = FilterButton()
+        button.setTitle("Alphabet", for: .normal)
+        return button
+    }()
+    
     init(){
         super.init(nibName: "FilterController", bundle: Bundle(for: FilterController.self))
         self.modalPresentationStyle = .overCurrentContext
@@ -21,6 +54,24 @@ class FilterController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    @objc
+    func dateCreateButtonTapped(){
+        dateCreateButton.flag = !dateCreateButton.flag
+        if dateCreateButton.flag{
+            alphabetButton.flag = false
+        }
+    }
+    
+    @objc
+    func alphabetButtonTapped(){
+        alphabetButton.flag = !alphabetButton.flag
+        if alphabetButton.flag{
+            dateCreateButton.flag = false
+        }
+    }
+    
+    
     
     func showFilter(){
         if #available(iOS 13, *) {
@@ -33,11 +84,21 @@ class FilterController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBaseView()
+        setupStackView()
+        dateCreateButton.addTarget(self, action: #selector(dateCreateButtonTapped), for: .touchUpInside)
+        alphabetButton.addTarget(self, action: #selector(alphabetButtonTapped), for: .touchUpInside)
+        dateCreateButton.flag = true
     }
     
     private func setupBaseView(){
         baseView.layer.masksToBounds = true
         baseView.layer.cornerRadius = 20
     }
+    
+    private func setupStackView(){
+        stackView.addArrangedSubview(dateCreateButton)
+        stackView.addArrangedSubview(alphabetButton)
+    }
+    
 
 }
